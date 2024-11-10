6e4f188e718233e7c3087977e36dde145613b17e
Benoît des Ligneris <bligneri@users.noreply.github.com>
2024-11-10 09:56:26 -0500

    add AMPQ and test server connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	var rabbitQueue = "amqp://guest:guest@localhost:5672"
+
+	con, err := amqp.Dial(rabbitQueue)
+	if err != nil {
+		fmt.Println("Error :", err)
+		return
+	}
+	defer con.Close()
 }
